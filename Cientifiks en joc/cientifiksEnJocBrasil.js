a10be8c3e63bff00e445dfe6214f90d4d9c3400c
MarcPlateroPolitecnics <145032302+MarcPlateroPolitecnics@users.noreply.github.com>
2023-10-17 18:49:31 +0200

    Update cientifiksEnJocBrasil.js

--- Cientifiks en joc/cientifiksEnJocBrasil.js
+++ Cientifiks en joc/cientifiksEnJocBrasil.js
@@ -1,80 +1,79 @@
 document.addEventListener("DOMContentLoaded", function() {
+  const gameContainer = document.getElementById("window");
+  const player = document.getElementById("player");
+  const fruit = document.getElementById("fruit");
+  const scoreValue = document.getElementById("scoreValue");
+  const livesValue = document.getElementById("livesValue");
   
-    const playerSize = 50;
-    const fruitSize = 30;
-    const mypantalla = document.getElementById("pantalla");
-    const initialPlayerPos = [mypantalla.style.width() / 2 - playerSize / 2, mypantalla.style.height() - playerSize - 10];
-    const initialFruitPos = [Math.random() * (canvas.width - fruitSize), 0];
-    const fruitSpeed = 5;
-    let playerPos = [...initialPlayerPos];
-    let fruitPos = [...initialFruitPos];
-    let score = 0;
-    let lives = 5;
-    let startTime = Date.now();
-    const gameDuration = 180000; // 3 mins en mil·lisegons.
-  
-    function draw() {
-      
-      // Dibuxar jugador.
-      ctx.fillStyle = "#000000";
-      ctx.fillRect(playerPos[0], playerPos[1], playerSize, playerSize);
-  
-      // Dibuxar fruita.
-      ctx.fillStyle = "#f00";
-      ctx.fillRect(fruitPos[0], fruitPos[1], fruitSize, fruitSize);
-  
-      // Dibuxar text.
-      ctx.fillStyle = "#000000";
-      ctx.font = "20px Arial";
-      ctx.fillText(`Score: ${score} | Lives: ${lives}`, 10, 30);
-  
-      // Moure fruita.
-      fruitPos[1] += fruitSpeed;
-  
-      // Verificar col·lisió.
-      if (
-        playerPos[0] < fruitPos[0] + fruitSize &&
-        playerPos[0] + playerSize > fruitPos[0] &&
-        playerPos[1] < fruitPos[1] + fruitSize &&
-        playerPos[1] + playerSize > fruitPos[1]
-      ) {
-        score++;
-        fruitPos = [Math.random() * (canvas.width - fruitSize), 0];
-      }
-  
-      // Verificar caiguda de fruita al terra.
-      if (fruitPos[1] > canvas.height) {
-        fruitPos = [Math.random() * (canvas.width - fruitSize), 0];
-        lives--;
-      }
-  
-      // Verificar temps i vides.
-      const elapsed = Date.now() - startTime;
-      if (elapsed >= gameDuration || lives <= 0) {
-        restartGame();
-      }
-  
-      requestAnimationFrame(draw);
-    }
-  
-    function restartGame() {
-      playerPos = [...initialPlayerPos];
-      fruitPos = [...initialFruitPos];
-      score = 0;
-      lives = 5;
-      startTime = Date.now();
-    }
-    
-    document.addEventListener("keydown", function(e) {
-        const movementSpeed = 50; // Augmentar aquest valor per així augmentar la velocitat.
-        
-        if (e.key === "ArrowLeft" && playerPos[0] > 0) {
-          playerPos[0] -= movementSpeed;
-        } else if (e.key === "ArrowRight" && playerPos[0] < canvas.width - playerSize) {
-          playerPos[0] += movementSpeed;
-        }
-    });
+  const playerSize = 50;
+  const fruitSize = 30;
+  const initialPlayerPos = [gameContainer.clientWidth / 2 - playerSize / 2, gameContainer.clientHeight - playerSize - 10];
+  const initialFruitPos = [Math.random() * (gameContainer.clientWidth - fruitSize), 0];
+  const fruitSpeed = 5;
+  let playerPos = [...initialPlayerPos];
+  let fruitPos = [...initialFruitPos];
+  let score = 0;
+  let lives = 5;
+  let startTime = Date.now();
+  const gameDuration = 180000; // 3 mins en mil·lisegons.
 
-    restartGame();
-    draw();
+  function draw() {
+    player.style.left = `${playerPos[0]}px`;
+    player.style.top = `${playerPos[1]}px`;
+    
+    fruit.style.left = `${fruitPos[0]}px`;
+    fruit.style.top = `${fruitPos[1]}px`;
+    
+    scoreValue.textContent = score;
+    livesValue.textContent = lives;
+
+    // Moure fruita.
+    fruitPos[1] += fruitSpeed;
+
+    // Verificar col·lisió.
+    if (
+      playerPos[0] < fruitPos[0] + fruitSize &&
+      playerPos[0] + playerSize > fruitPos[0] &&
+      playerPos[1] < fruitPos[1] + fruitSize &&
+      playerPos[1] + playerSize > fruitPos[1]
+    ) {
+      score++;
+      fruitPos = [Math.random() * (gameContainer.clientWidth - fruitSize), 0];
+    }
+
+    // Verificar caiguda de fruita al terra.
+    if (fruitPos[1] > gameContainer.clientHeight) {
+      fruitPos = [Math.random() * (gameContainer.clientWidth - fruitSize), 0];
+      lives--;
+    }
+
+    // Verificar temps i vides.
+    const elapsed = Date.now() - startTime;
+    if (elapsed >= gameDuration || lives <= 0) {
+      restartGame();
+    }
+
+    requestAnimationFrame(draw);
+  }
+
+  function restartGame() {
+    playerPos = [...initialPlayerPos];
+    fruitPos = [...initialFruitPos];
+    score = 0;
+    lives = 5;
+    startTime = Date.now();
+  }
+  
+  document.addEventListener("keydown", function(e) {
+      const movementSpeed = 50; // Augmentar aquest valor per així augmentar la velocitat del jugador.
+      
+      if (e.key === "ArrowLeft" && playerPos[0] > 0) {
+        playerPos[0] -= movementSpeed;
+      } else if (e.key === "ArrowRight" && playerPos[0] < gameContainer.clientWidth - playerSize) {
+        playerPos[0] += movementSpeed;
+      }
   });
+
+  restartGame();
+  draw();
+});
