cd319b6132405faf343fc95e2056c60c53ddbda3
MarcPlateroPolitecnics <145032302+MarcPlateroPolitecnics@users.noreply.github.com>
2023-10-26 17:19:01 +0200

    Update cientifiksEnJocBrasil.js

--- Cientifiks en joc/cientifiksEnJocBrasil.js
+++ Cientifiks en joc/cientifiksEnJocBrasil.js
@@ -1,14 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const gameContainer = document.getElementById("window");
   const player = document.getElementById("player");
   const fruit = document.getElementById("fruit");
   const scoreValue = document.getElementById("scoreValue");
   const livesValue = document.getElementById("livesValue");
-  
+
   const playerSize = 50;
   const fruitSize = 30;
-  const initialPlayerPos = [gameContainer.clientWidth / 2 - playerSize / 2, gameContainer.clientHeight - playerSize - 10];
-  const initialFruitPos = [Math.random() * (gameContainer.clientWidth - fruitSize), 0];
+  const initialPlayerPos = [
+    gameContainer.clientWidth / 2 - playerSize / 2,
+    gameContainer.clientHeight - playerSize - 10,
+  ];
+  const initialFruitPos = [
+    Math.random() * (gameContainer.clientWidth - fruitSize),
+    0,
+  ];
   const fruitSpeed = 5;
   let playerPos = [...initialPlayerPos];
   let fruitPos = [...initialFruitPos];
@@ -20,10 +26,10 @@ document.addEventListener("DOMContentLoaded", function() {
   function draw() {
     player.style.left = `${playerPos[0]}px`;
     player.style.top = `${playerPos[1]}px`;
-    
+
     fruit.style.left = `${fruitPos[0]}px`;
     fruit.style.top = `${fruitPos[1]}px`;
-    
+
     scoreValue.textContent = score;
     livesValue.textContent = lives;
 
@@ -38,13 +44,22 @@ document.addEventListener("DOMContentLoaded", function() {
       playerPos[1] + playerSize > fruitPos[1]
     ) {
       score++;
-      fruitPos = [Math.random() * (gameContainer.clientWidth - fruitSize), 0];
+      fruitPos = [
+        Math.random() * (gameContainer.clientWidth - fruitSize),
+        0,
+      ];
     }
 
     // Verificar caiguda de fruita al terra.
     if (fruitPos[1] > gameContainer.clientHeight) {
-      fruitPos = [Math.random() * (gameContainer.clientWidth - fruitSize), 0];
-      lives--;
+      if (fruitPos[1] + fruitSize > gameContainer.clientHeight) {
+        // Restar vida només si la part inferior de la fruta toca el marc.
+        lives--;
+        fruitPos = [
+          Math.random() * (gameContainer.clientWidth - fruitSize),
+          0,
+        ];
+      }
     }
 
     // Verificar temps i vides.
@@ -63,15 +78,18 @@ document.addEventListener("DOMContentLoaded", function() {
     lives = 5;
     startTime = Date.now();
   }
-  
-  document.addEventListener("keydown", function(e) {
-      const movementSpeed = 50; // Augmentar aquest valor per així augmentar la velocitat del jugador.
-      
-      if (e.key === "ArrowLeft" && playerPos[0] > 0) {
-        playerPos[0] -= movementSpeed;
-      } else if (e.key === "ArrowRight" && playerPos[0] < gameContainer.clientWidth - playerSize) {
-        playerPos[0] += movementSpeed;
-      }
+
+  document.addEventListener("keydown", function (e) {
+    const movementSpeed = 50; // Augmentar aquest valor per així augmentar la velocitat del jugador.
+
+    if (e.key === "ArrowLeft" && playerPos[0] > 0) {
+      playerPos[0] -= movementSpeed;
+    } else if (
+      e.key === "ArrowRight" &&
+      playerPos[0] < gameContainer.clientWidth - playerSize
+    ) {
+      playerPos[0] += movementSpeed;
+    }
   });
 
   restartGame();
