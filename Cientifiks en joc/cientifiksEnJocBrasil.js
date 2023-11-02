d3f66a50326645b530e277336ab89b1ba54f76fa
MarcPlateroPolitecnics <145032302+MarcPlateroPolitecnics@users.noreply.github.com>
2023-11-02 20:13:59 +0100

    Update cientifiksEnJocBrasil.js

--- Cientifiks en joc/cientifiksEnJocBrasil.js
+++ Cientifiks en joc/cientifiksEnJocBrasil.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const scoreValue = document.getElementById("scoreValue");
   const livesValue = document.getElementById("livesValue");
 
-  const playerSize = 50;
+  const playerSize = 50;  
   const fruitSize = 30;
   const initialPlayerPos = [
     gameContainer.clientWidth / 2 - playerSize / 2,
@@ -20,8 +20,8 @@ document.addEventListener("DOMContentLoaded", function () {
   let fruitPos = [...initialFruitPos];
   let score = 0;
   let lives = 5;
-  let startTime = Date.now();
-  const gameDuration = 180000; // 3 mins en mil·lisegons.
+  let doubleFruits = false; // Nova variable per controlar les dues fruites.
+  const doubleFruitThreshold = 10; // Puntuació per activar les dues fruites.
 
   function draw() {
     player.style.left = `${playerPos[0]}px`;
@@ -44,10 +44,18 @@ document.addEventListener("DOMContentLoaded", function () {
       playerPos[1] + playerSize > fruitPos[1]
     ) {
       score++;
-      fruitPos = [
-        Math.random() * (gameContainer.clientWidth - fruitSize),
-        0,
-      ];
+
+      if (score === doubleFruitThreshold && !doubleFruits) {
+        // Activa les dues fruites quan s'arriba a 10 punts.
+        doubleFruits = true;
+        spawnDoubleFruits();
+        changeBackground();
+      } else {
+        fruitPos = [
+          Math.random() * (gameContainer.clientWidth - fruitSize),
+          0,
+        ];
+      }
     }
 
     // Verificar caiguda de fruita al terra.
@@ -62,13 +70,57 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     }
 
-    // Verificar temps i vides.
-    const elapsed = Date.now() - startTime;
-    if (elapsed >= gameDuration || lives <= 0) {
-      restartGame();
+    requestAnimationFrame(draw);
+  }
+
+  function spawnDoubleFruits() {
+    // Genera dues posicions aleatòries per a les dues fruites
+    const secondFruitPos = [
+      Math.random() * (gameContainer.clientWidth - fruitSize),
+      0,
+    ];
+
+    // Crea una segona fuita
+    const secondFruit = document.createElement("img");
+    secondFruit.src = "C:/Users/cep.ID21090261/Desktop/Descargar Xampp/xampp/htdocs/M12/img/banana.png";
+    secondFruit.style.position = "absolute";
+    secondFruit.style.width = `${fruitSize}px`;
+    secondFruit.style.height = `${fruitSize}px`;
+    gameContainer.appendChild(secondFruit);
+
+    function drawSecondFruit() {
+      secondFruit.style.left = `${secondFruitPos[0]}px`;
+      secondFruit.style.top = `${secondFruitPos[1]}px`;
+      secondFruitPos[1] += fruitSpeed * 1.5; // Ajusta la velocitat de la segona fuita.
+
+      if (
+        playerPos[0] < secondFruitPos[0] + fruitSize &&
+        playerPos[0] + playerSize > secondFruitPos[0] &&
+        playerPos[1] < secondFruitPos[1] + fruitSize &&
+        playerPos[1] + playerSize > secondFruitPos[1]
+      ) {
+        // Incrementa la puntuació quan es recull la segona fuita.
+        score++;
+        secondFruitPos = [
+          Math.random() * (gameContainer.clientWidth - fruitSize),
+          0,
+        ];
+      }
+
+      // Verifica si la segona fuita ha caigut al terra.
+      if (secondFruitPos[1] > gameContainer.clientHeight) {
+        gameContainer.removeChild(secondFruit);
+      } else {
+        requestAnimationFrame(drawSecondFruit);
+      }
     }
 
-    requestAnimationFrame(draw);
+    drawSecondFruit();
+  }
+
+  function changeBackground() {
+    // Canvia el fons després d'aconseguir els 10 punts.
+    document.getElementById("background1").src = "C:/Users/cep.ID21090261/Desktop/Descargar Xampp/xampp/htdocs/M12/img/brasilFondo2.jpg";
   }
 
   function restartGame() {
@@ -76,7 +128,6 @@ document.addEventListener("DOMContentLoaded", function () {
     fruitPos = [...initialFruitPos];
     score = 0;
     lives = 5;
-    startTime = Date.now();
   }
 
   document.addEventListener("keydown", function (e) {
